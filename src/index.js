3b3fd00e9431af7ce1864e5e1880bfb393d30dc6
hyeonw8 <whgusrud223@gmail.com>
2023-01-05 00:55:40 +0900

    add commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/compat/app';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
