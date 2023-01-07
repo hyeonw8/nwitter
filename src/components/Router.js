dae85484e5099e366d7abcb07047d20641840885
hyeonw8 <whgusrud223@gmail.com>
2023-01-08 00:29:36 +0900

    #2 commit

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,16 +1,23 @@
 import React, { useState } from "react";
-import { HashRouter as Router, Route, Switch  } from "react-router-dom";
+import { HashRouter as Router, Redirect, Route, Switch  } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
+
 
 const AppRouter = ({ isLoggedIn }) => {
-    return (
-        <Router>
+    return ( //&&은 navigation이 존재하려면 isloggedin이 true여야 한다는 것
+        <Router> 
+            {isLoggedIn && <Navigation />} 
             <Switch>
                 {isLoggedIn ? (
                     <>
                     <Route exact path="/">
                         <Home />
+                    </Route>
+                    <Route exact path="/profile">
+                        <Profile />
                     </Route>
                     </> 
                 )    :  (
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,13 @@
+import { authService, firebaseInstance } from "fbase";
 import React, { useState } from "react";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, GoogleAuthProvider,signInWithPopup} from 'firebase/auth'
+import { async } from "@firebase/util";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
     const onchange = (event) => {
         const {
             target: {name, value},
@@ -13,21 +18,50 @@ const Auth = () => {
             setPassword(value);
         }
     };
-    const onSubmit = (event) => {
-        event.preventDefault();
+    const onSubmit = async (event) => {
+        event.preventDefault(); //기본 행위가 실행되는 걸 원치 않음, 내가 컨트롤 할 수 있게 함
+        try {
+            let data;
+            const auth = getAuth();
+            if(newAccount) {
+                const data = await createUserWithEmailAndPassword(authService,email, password);
+            } else {
+                const data = await signInWithEmailAndPassword(authService,email, password);
+            }
+            console.log(data)
+        } catch (error) {
+            setError(error.message.replace("Firebase:",""));
+        }
     };
+    const toggleAccount = () => setNewAccount((prev) => !prev);
+    const onSocialClick = async (event) => {
+        const {
+            target: {name},
+        } = event;
+        let provider;
+        if(name === "google"){
+            provider = new GoogleAuthProvider();
+        } 
+        const data = await signInWithPopup(authService, provider);
+        console.log(data);
+    };
+
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input name="email" type="text" placeholder="Email" required value={email} onChange={onchange} />
+                <input name="email" type="email" placeholder="Email" required value={email} onChange={onchange} />
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onchange} />
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
+                {error}  
             </form>
+            <span onClick={toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
-                <button>Continue with Google</button>
+                <button onClick={onSocialClick} name="google">Continue with Google</button>
                 <button>Continue with Github</button>
             </div>
-    </div>
+        </div>
     );
 }
     
