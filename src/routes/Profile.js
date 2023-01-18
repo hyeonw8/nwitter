e69ea4f965082376518590b1371359934eeee112
hyeonw8 <whgusrud223@gmail.com>
2023-01-18 23:49:33 +0900

    profile page

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,15 @@
-import { authService, dbService } from "fbase";
-import React, { useEffect, useState } from "react";
+import { authService, dbService, storageService } from "fbase";
+import React, { useEffect, useReducer, useState } from "react";
 import { useHistory } from "react-router-dom";
 import {collection, getDocs, query, where, orderBy} from "@firebase/firestore";
 import { updateProfile } from "firebase/auth";
 import { async } from "@firebase/util";
-import ProfilePhoto from "components/ProfilePhoto";
+import { getDownloadURL } from "firebase/storage";
 
-export default ({ refreshUser,userObj}) => {
+const Profile = ({ refreshUser,userObj}) => {
     const history = useHistory();
     const [newDisplayName, setNewDisplayName] = useState(userObj.newDisplayName);
+    const [newPhoto, setNewPhoto] = useState(userObj.photoURL);
     const onLogOutClick = () => {
       authService.signOut();
       history.push("/");
@@ -28,29 +29,84 @@ export default ({ refreshUser,userObj}) => {
     useEffect(() => {
       getMyNweets();
     }, []) */
-    const onChange = (event) => {
+    const onChangName = (event) => {
       const {
         target : {value},
       } = event;
       setNewDisplayName(value);
     };
     
+    const onChangePhoto = e => {
+      const {target : {value}} = e;
+      const photo = files[0];
+      const reader = new FileReader();
+      reader.onloadend = (finishedEvent) => {
+        const {currentTarget : {result} }  = finishedEvent;
+            setnewPhoto(result); //onloadend에 finishEvent의 result를 setAttachment로 설정
+        }
+        reader.readAsDataURL(photo);
+      }
+    
     const onSubmit = async(event) => {
       event.preventDefault();
+      let photoURL = userObj.photoURL;
+      if(newPhoto !== userObj.photoURL) {
+        const photoRef = ref(storageService,`${userObj.uid}/profile/photo` );
+        const response = await uploadString(photoRef, "data_url");
+        photoURL = await getDownloadURL(response.ref);
+      }
       if(userObj.displayName !== newDisplayName){ //아무 변경사항 없으면 업데이트를 하지 않음
         await updateProfile(authService.currentUser,
-         {displayName: newDisplayName}
+        {displayName: newDisplayName, }
           //console.log(userObj.updateProfile);
       )}; 
       refreshUser(); //react.js에 있는 profile 새로고침~
     }
+    const onClearPhoto  = () => {
+      setNewPhoto("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png");
+    };
+  
     
     return (
       <div className="container">
         <form onSubmit={onSubmit} className="profileForm">
-          <ProfilePhoto />
+        <>
+        {upimage && (
+                <div>
+                    <Avatar 
+                        className="avatar_img"
+                        src={newPhoto} 
+                        size={200} 
+                        onClick={()=>{fileInput.current.click()}}/>
+                    <div 
+                        className="factoryForm__clear" 
+                        onClick={onClearPhoto}>
+                        <span>Remove</span>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </div>
+                </div>
+            )} 
+          </>
+            <input 
+                id="add-profimg"
+                type="file"
+                accept="image/*"
+                name='profile_img'
+                onChange={onChangePhoto}
+                ref={fileInput}          
+                style={{
+                    opacity: 0,
+                }}/>
+            <input 
+                type="submit" 
+                value="Update Image"
+                className="formBtn"
+                style={{
+                marginBottom: 10,
+                }} 
+            />
           <input 
-            onChange={onChange}
+            onChange={onChangName}
             type="text" 
             autoFocus
             placeholder="Display name" 
@@ -59,7 +115,7 @@ export default ({ refreshUser,userObj}) => {
           />
           <input 
             type="submit" 
-            value="Update Name"
+            value="Update Profile"
             className="formBtn"
             style={{
               marignTop: 10,
@@ -73,3 +129,4 @@ export default ({ refreshUser,userObj}) => {
     );
   };
 
+  export default Profile;
